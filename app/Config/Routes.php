77259a6b7e02d58e696004bdf7f29a8c937655db
Wesley Macedo <wesleys97.dev@gmail.com>
2026-08-03 03:34:39 -0300

    Atualizacao em 03/08/2026 03:34

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->post('push/salvar-inscricao', 'Push::salvarInscricao');
 
 // Rota para o disparo externo de push sem login
-$routes->post('api/push/enviar', 'Api\PushApi::enviar');
+$routes->post('api/push/enviar', 'api\PushApi::enviar'); 
